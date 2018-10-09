6ca42cb1fbdc8d5bde58e3cf67b1fb3a0bfba544
Hendrik Makait <hendrik.makait@gmail.com>
2018-10-09 17:15:04 +0200

    Add editline support for all OS

--- prompt.c
+++ prompt.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for user input of size 2048 */
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
 
-static char input[2048];
+static char buffer[2048];
 
-int main(int argc, char** argv) {
+/* Fake readline function */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer) + 1);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+
+/* Otherwise incluse the editline headers */
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+
+int main(int argc, char **argv) {
 
     /* Print Version and Exit Information */
     puts("Lispy Version 0.0.0.0.1");
@@ -13,14 +36,17 @@ int main(int argc, char** argv) {
     /* In a never ending loop */
     while (1) {
 
-        /* Output our prompt */
-        fputs("lispy> ", stdout);
+        /* Output our prompt and get input*/
+        char *input = readline("lispy> ");
 
-        /* Read a line of user input of maximum size 2048 */
-        fgets(input, 2048, stdin);
+        /* Add input to history */
+        add_history(input);
 
         /* Echo input back to user */
-        printf("No you're a %s", input);
+        printf("No you're a %s\n", input);
+
+        /* Free retrived input */
+        free(input);
     }
 
     return 0;
